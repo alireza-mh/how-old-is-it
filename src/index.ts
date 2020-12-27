89dfedb9ee9423026ae16a04b6a4e51c018e9981
Alireza <alireza.mh@gmail.com>
2020-12-27 22:07:43 +0330

    feat: get all days - add date-fns

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 const exec = require('child_process').exec;
+import { parse, differenceInMonths, differenceInYears, differenceInDays } from 'date-fns' 
+
 function execute(command, callback){
     exec(command, function(error, stdout, stderr){ callback(stdout); });
 };
 
-execute('git log --reverse', (output) => console.log(output));
+/* using git is much faster than using nodegit */
+execute('git log --reverse', (output) => {
+    const allDates = output.match(/Date:(.*)/gi);
+});
